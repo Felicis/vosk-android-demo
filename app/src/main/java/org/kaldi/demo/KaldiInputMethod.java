560211a3b104175efa84e1170494f3089678ccc3
Your Name <you@example.com>
2021-02-22 07:52:09 +0100

    layout with punctuation

--- app/src/main/java/org/kaldi/demo/KaldiInputMethod.java
+++ app/src/main/java/org/kaldi/demo/KaldiInputMethod.java
@@ -22,6 +22,7 @@ import android.text.method.ScrollingMovementMethod;
 import android.util.Log;
 import android.view.View;
 import android.view.inputmethod.EditorInfo;
+import android.view.inputmethod.InputConnection;
 import android.widget.Button;
 import android.widget.TextView;
 
@@ -35,7 +36,6 @@ import org.kaldi.Vosk;
 
 import java.io.File;
 import java.io.IOException;
-import java.io.InputStream;
 import java.lang.ref.WeakReference;
 
 import androidx.core.content.ContextCompat;
@@ -99,12 +99,12 @@ public class KaldiInputMethod extends InputMethodService implements
         resultView = overlayView.findViewById(R.id.result_text);
         setUiState(STATE_START);
 
-        overlayView.findViewById(R.id.recognize_file).setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                recognizeFile();
-            }
-        });
+//        overlayView.findViewById(R.id.recognize_file).setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View view) {
+//                recognizeFile();
+//            }
+//        });
 
         overlayView.findViewById(R.id.recognize_mic).setOnClickListener(new View.OnClickListener() {
             @Override
@@ -112,7 +112,65 @@ public class KaldiInputMethod extends InputMethodService implements
                 recognizeMicrophone();
             }
         });
+        overlayView.findViewById(R.id.world).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // world
+            }
+        });
+        overlayView.findViewById(R.id.backspace).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // delete last char (long press?)
+                InputConnection ic = getCurrentInputConnection();
+                if (ic != null)
+                    ic.deleteSurroundingText(1, 0);
+            }
+        });
+        overlayView.findViewById(R.id.colon).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                appendToComposing(":");
+            }
+        });
+        overlayView.findViewById(R.id.exclamation).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                appendToComposing("!");
+            }
+        });
+        overlayView.findViewById(R.id.question).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                appendToComposing("?");
+            }
+        });
+        overlayView.findViewById(R.id.comma).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                appendToComposing(",");
+            }
+        });
+        overlayView.findViewById(R.id.dot).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                appendToComposing(".");
+            }
+        });
+        overlayView.findViewById(R.id.enter).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                appendToComposing("\n");
+            }
+        });
+
         return overlayView;
+    }
+
+    private void appendToComposing(String text) {
+        InputConnection ic = getCurrentInputConnection();
+        if (ic != null)
+            ic.commitText(text, 1);
     }
 
     private static class SetupTask extends AsyncTask<Void, Void, Exception> {
@@ -149,49 +207,49 @@ public class KaldiInputMethod extends InputMethodService implements
         }
     }
 
-    private static class RecognizeTask extends AsyncTask<Void, Void, String> {
-        WeakReference<KaldiInputMethod> activityReference;
-        WeakReference<TextView> resultView;
-
-        RecognizeTask(KaldiInputMethod activity, TextView resultView) {
-            this.activityReference = new WeakReference<>(activity);
-            this.resultView = new WeakReference<>(resultView);
-        }
-
-        @Override
-        protected String doInBackground(Void... params) {
-            KaldiRecognizer rec;
-            long startTime = System.currentTimeMillis();
-            StringBuilder result = new StringBuilder();
-            try {
-                rec = new KaldiRecognizer(activityReference.get().model, 16000.f, "[\"oh zero one two three four five six seven eight nine\"]");
-
-                InputStream ais = activityReference.get().getAssets().open("10001-90210-01803.wav");
-                if (ais.skip(44) != 44) {
-                    return "";
-                }
-                byte[] b = new byte[4096];
-                int nbytes;
-                while ((nbytes = ais.read(b)) >= 0) {
-                    if (rec.AcceptWaveform(b, nbytes)) {
-                        result.append(rec.Result());
-                    } else {
-                        result.append(rec.PartialResult());
-                    }
-                }
-                result.append(rec.FinalResult());
-            } catch (IOException e) {
-                return "";
-            }
-            return String.format(activityReference.get().getString(R.string.elapsed), result.toString(), (System.currentTimeMillis() - startTime));
-        }
-
-        @Override
-        protected void onPostExecute(String result) {
-            activityReference.get().setUiState(STATE_READY);
-            resultView.get().append(result + "\n");
-        }
-    }
+//    private static class RecognizeTask extends AsyncTask<Void, Void, String> {
+//        WeakReference<KaldiInputMethod> activityReference;
+//        WeakReference<TextView> resultView;
+//
+//        RecognizeTask(KaldiInputMethod activity, TextView resultView) {
+//            this.activityReference = new WeakReference<>(activity);
+//            this.resultView = new WeakReference<>(resultView);
+//        }
+//
+//        @Override
+//        protected String doInBackground(Void... params) {
+//            KaldiRecognizer rec;
+//            long startTime = System.currentTimeMillis();
+//            StringBuilder result = new StringBuilder();
+//            try {
+//                rec = new KaldiRecognizer(activityReference.get().model, 16000.f, "[\"oh zero one two three four five six seven eight nine\"]");
+//
+//                InputStream ais = activityReference.get().getAssets().open("10001-90210-01803.wav");
+//                if (ais.skip(44) != 44) {
+//                    return "";
+//                }
+//                byte[] b = new byte[4096];
+//                int nbytes;
+//                while ((nbytes = ais.read(b)) >= 0) {
+//                    if (rec.AcceptWaveform(b, nbytes)) {
+//                        result.append(rec.Result());
+//                    } else {
+//                        result.append(rec.PartialResult());
+//                    }
+//                }
+//                result.append(rec.FinalResult());
+//            } catch (IOException e) {
+//                return "";
+//            }
+//            return String.format(activityReference.get().getString(R.string.elapsed), result.toString(), (System.currentTimeMillis() - startTime));
+//        }
+//
+//        @Override
+//        protected void onPostExecute(String result) {
+//            activityReference.get().setUiState(STATE_READY);
+//            resultView.get().append(result + "\n");
+//        }
+//    }
 
 
     @Override
@@ -207,38 +265,35 @@ public class KaldiInputMethod extends InputMethodService implements
 
     @Override
     public void onResult(String hypothesis) {
-        //resultView.append(hypothesis + "\n");
         System.out.println(hypothesis);
         try {
             JSONObject result = new JSONObject(hypothesis);
             String text = result.getString("text");
-            String finalText = " " + text;
+            String finalText = text;
             switch (text) {
                 case "punkt":
                     finalText = ".";
                     break;
             }
-            textAccumulator += finalText;
-            resultView.setText(textAccumulator);
+//            resultView.setText(finalText);
+            InputConnection ic = getCurrentInputConnection();
+            if (ic != null)
+                ic.commitText(finalText, 1);
         } catch (Exception e) {
             System.out.println("ERROR: Json parse exception");
         }
-        //InputConnection ic = getCurrentInputConnection();
-        //ic.deleteSurroundingText(4, 0);
-        //ic.commitText("Hello", 1);
-        //ic.commitText("!", 1);
 
     }
 
     @Override
     public void onPartialResult(String hypothesis) {
-        //resultView.append(hypothesis + "\n");
         System.out.println(hypothesis);
         try {
             JSONObject partialResult = new JSONObject(hypothesis);
             String partialText = partialResult.getString("partial");
-            String tempText = textAccumulator + " " + partialText;
-            resultView.setText(tempText);
+//            resultView.setText(partialText);
+            InputConnection ic = getCurrentInputConnection();
+            ic.setComposingText(partialText, 1);
         } catch (Exception e) {
             System.out.println("ERROR: Json parse exception");
         }
@@ -261,29 +316,29 @@ public class KaldiInputMethod extends InputMethodService implements
             case STATE_START:
                 resultView.setText(R.string.preparing);
                 resultView.setMovementMethod(new ScrollingMovementMethod());
-                overlayView.findViewById(R.id.recognize_file).setEnabled(false);
+//                overlayView.findViewById(R.id.recognize_file).setEnabled(false);
                 overlayView.findViewById(R.id.recognize_mic).setEnabled(false);
                 break;
             case STATE_READY:
                 resultView.setText(R.string.ready);
                 ((Button) overlayView.findViewById(R.id.recognize_mic)).setText(R.string.recognize_microphone);
-                overlayView.findViewById(R.id.recognize_file).setEnabled(true);
+//                overlayView.findViewById(R.id.recognize_file).setEnabled(true);
                 overlayView.findViewById(R.id.recognize_mic).setEnabled(true);
                 break;
             case STATE_DONE:
                 ((Button) overlayView.findViewById(R.id.recognize_mic)).setText(R.string.recognize_microphone);
-                overlayView.findViewById(R.id.recognize_file).setEnabled(true);
+//                overlayView.findViewById(R.id.recognize_file).setEnabled(true);
                 overlayView.findViewById(R.id.recognize_mic).setEnabled(true);
                 break;
             case STATE_FILE:
                 resultView.setText(getString(R.string.starting));
                 overlayView.findViewById(R.id.recognize_mic).setEnabled(false);
-                overlayView.findViewById(R.id.recognize_file).setEnabled(false);
+//                overlayView.findViewById(R.id.recognize_file).setEnabled(false);
                 break;
             case STATE_MIC:
                 ((Button) overlayView.findViewById(R.id.recognize_mic)).setText(R.string.stop_microphone);
                 resultView.setText(getString(R.string.say_something));
-                overlayView.findViewById(R.id.recognize_file).setEnabled(false);
+//                overlayView.findViewById(R.id.recognize_file).setEnabled(false);
                 overlayView.findViewById(R.id.recognize_mic).setEnabled(true);
                 break;
         }
@@ -292,14 +347,14 @@ public class KaldiInputMethod extends InputMethodService implements
     private void setErrorState(String message) {
         resultView.setText(message);
         ((Button) overlayView.findViewById(R.id.recognize_mic)).setText(R.string.recognize_microphone);
-        overlayView.findViewById(R.id.recognize_file).setEnabled(false);
+//        overlayView.findViewById(R.id.recognize_file).setEnabled(false);
         overlayView.findViewById(R.id.recognize_mic).setEnabled(false);
     }
 
-    public void recognizeFile() {
-        setUiState(STATE_FILE);
-        new RecognizeTask(this, resultView).execute();
-    }
+//    public void recognizeFile() {
+//        setUiState(STATE_FILE);
+//        new RecognizeTask(this, resultView).execute();
+//    }
 
     public void recognizeMicrophone() {
         if (speechService != null) {
